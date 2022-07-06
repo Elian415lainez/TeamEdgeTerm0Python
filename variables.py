5d99b4535058e2e2c9ea65f4b5a4e4a0a9da9107
Elian_L <elian.lainez@csedge.org>
2022-07-06 14:44:50 -0700

    my first commit

--- variables.py
+++ variables.py
@@ -16,6 +16,10 @@ print(message)
 
 print("------------------- Challenge 1 -------------------")
 
+name = "elian lainez"
+grade = 11
+funFact = "I am a defender in soccer"
+
 print(f"Hi! My name is {name}")
 print(f"I'm in the {grade}th grade.")
 print(f"A fun fact about me is that {funFact}")
@@ -29,15 +33,25 @@ print(f"A fun fact about me is that {funFact}")
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
-
-
+print(type(name))
+print(type(grade))
+print(type(funFact))
 
 # **** Challenge 1: Problem 2 ****
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
+line1 = "happy birthday to you"
+line2 = "happy birthday to you"
+line3 = "happy birthday dear elian"
+line4 = "happy birthday to YOUUUUU!!"
+line5 = "woooooo"
 
-
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
@@ -60,32 +74,42 @@ num2 = 10
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
-
-
+sum = num1 +  num2
+print(sum)
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
 
-
+difference = sum - 7
+print(difference)
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
 
+product = difference * 3
+print(product)
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
+
+power = product**2
+print(power)
 
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
 
+quotient =  power/4
+print(quotient)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
 
+remainder = quotient % 2
+print(remainder)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
@@ -93,5 +117,9 @@ num2 = 10
 # Your code should work if the value of the variable is changed.
 
 num = 9
+before = num -1
+after = num +1
 
+print(before)
+print(after)
 # -------------------------------------------- 
